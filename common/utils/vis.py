36772ec980bd9e7e80b303dc6b49bce7509e1c9d
KGMSFT <kgmsft@163.com>
2019-03-01 22:00:06 +0800

    chang vis.py

--- common/utils/vis.py
+++ common/utils/vis.py
@@ -41,12 +41,24 @@ def vis_keypoints(img, kps, kps_lines, kp_thresh=0.4, alpha=1):
 def vis_3d_skeleton(kpt_3d, kpt_3d_vis, kps_lines, filename=None):
 
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = Axes3D(fig)
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax = Axes3D(fig)
+
+
+
 
     # Convert from plt 0-1 RGBA colors to 0-255 BGR colors for opencv.
     cmap = plt.get_cmap('rainbow')
     colors = [cmap(i) for i in np.linspace(0, 1, len(kps_lines) + 2)]
     colors = [np.array((c[2], c[1], c[0])) for c in colors]
+
+    minx = np.min(kpt_3d[:,0])
+    miny = np.min(kpt_3d[:,1])
+    minz = np.min(kpt_3d[:,2])
+
+    t = np.array([minx, miny, minz])
+    kpt_3d = kpt_3d - t
 
     for l in range(len(kps_lines)):
         i1 = kps_lines[l][0]
@@ -62,23 +74,45 @@ def vis_3d_skeleton(kpt_3d, kpt_3d_vis, kps_lines, filename=None):
         if kpt_3d_vis[i2,0] > 0:
             ax.scatter(kpt_3d[i2,0], kpt_3d[i2,2], -kpt_3d[i2,1], c=colors[l], marker='o')
 
-    x_r = np.array([0, cfg.input_shape[1]], dtype=np.float32)
-    y_r = np.array([0, cfg.input_shape[0]], dtype=np.float32)
-    z_r = np.array([0, 1], dtype=np.float32)
-    
-    if filename is None:
-        ax.set_title('3D vis')
-    else:
-        ax.set_title(filename)
+    # x_r = np.array([0, cfg.input_shape[1]], dtype=np.float32)
+    # y_r = np.array([0, cfg.input_shape[0]], dtype=np.float32)
+    # z_r = np.array([0, 1], dtype=np.float32)
 
+    xmin, xmax = ax.get_xlim3d()
+    ymin, ymax = ax.get_ylim3d()
+    zmin, zmax = ax.get_zlim3d()
+    print([xmin, xmax])
+    print([ymin, ymax])
+    print([zmin, zmax])
+    scale_x = xmax - xmin
+    scale_y = ymax - ymin
+    scale_z = zmax - zmin
+    scale = [scale_x/scale_z, scale_y/scale_z, 1.0, 1.0]
+    # scale = [0.9, 0.9, 1.0, 1.0]
+    print(np.diag(scale))
+    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag(scale))
+
+
+    
+    # if filename is None:
+    #     ax.set_title('3D vis')
+    # else:
+    #     ax.set_title(filename)
+    ax.set_title("3D")
     ax.set_xlabel('X Label')
     ax.set_ylabel('Z Label')
     ax.set_zlabel('Y Label')
-    #ax.set_xlim([0,cfg.input_shape[1]])
-    #ax.set_ylim([0,1])
-    #ax.set_zlim([-cfg.input_shape[0],0])
+    # ax.set_xticks([-x for x in range(0, 400) if x%50 == 0])
+    # ax.set_yticks([y for y in range(5500, 5850) if y%50 == 0])
+    # ax.set_zticks([z for z in range(0, 400) if z%50 == 0])
+    # ax.set_xlim([0,cfg.input_shape[1]])
+    # ax.set_ylim([0,1])
+    # ax.set_zlim([-400, 1000])
+    # ax.set_xlim([-400,1000])
+    # ax.set_ylim([5500,7000])
+    # ax.set_zlim([-400,1200])
     ax.legend()
-
+    # plt.savefig(filename)
     plt.show()
     cv2.waitKey(0)
 
